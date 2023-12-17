3398f32151141e0121514ae6753535e91cb76d7e
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2023-12-16 22:17:41 -0300

    comit teste

--- scripts.js
+++ scripts.js
@@ -1,82 +1,84 @@
-const button = document.querySelector('.button-add-task')
-const input = document.querySelector('.input-task')
-const listaCompleta = document.querySelector('.list-tasks')
+const button = document.querySelector('.button-add-processo')
+const input = document.querySelector('.input-processo')
+const listaCompleta = document.querySelector('.list-processos')
 
-let minhaListaDeItens = JSON.parse(localStorage.getItem('minhaListaDeItens')) || []
-
+let meusProcessos = JSON.parse(localStorage.getItem('meusProcessos')) || []
+/*
+minhalistaDeItens     = meusProcessos
+mostrarTarefas()      = mostrarProcessos()
+baixarTarefas()       = baixarProcessos()
+concluirTarefa()      = concluirProcesso()
+adicionarNovaTarefa() = adicionarNovoProcesso()
+*/
 window.onload = function() {
-  minhaListaDeItens = JSON.parse(localStorage.getItem('minhaListaDeItens')) || []
-  mostrarTarefas()
+  meusProcessos = JSON.parse(localStorage.getItem('meusProcessos')) || []
+  mostrarProcessos()
 }
 
-function baixarTarefas() {
-  const data = JSON.stringify(minhaListaDeItens, null, 2);
+function baixarProcessos() {
+  const data = JSON.stringify(meusProcessos/*minhaListaDeItens*/, null, 2);
   const blob = new Blob([data], {type: 'application/json'});
   const url = URL.createObjectURL(blob);
   const link = document.createElement('a');
   link.href = url;
-  link.download = 'tarefas.json';
+  link.download = 'processos.json';
   link.click();
   URL.revokeObjectURL(url);
 }
 
-function adicionarNovaTarefa() {
-  minhaListaDeItens.push({
-    tarefa: input.value,
-    concluida: false,
-    obs: '',
+function adicionarNovoProcesso() {
+  meusProcessos.push({
+    po: input.value,
+    concluido: false,
   })
 
   input.value = ''
-  localStorage.setItem('minhaListaDeItens', JSON.stringify(minhaListaDeItens))
-  mostrarTarefas()
+  localStorage.setItem('meusProcessos', JSON.stringify(meusProcessos))
+  mostrarProcessos()
 }
 
-function mostrarTarefas() {
+function mostrarProcessos() {
   let novaLi = `
     <tr id="titulo">
-      <th class="c1"></th>
-      <th class="c2">Nome</th>
-      <th class="c3">Prazo</th>
-      <th class="c4">Urgência</th>
-      <th class="c5">Obs.</th>
-      <th class="c6"></th>
+      <th class="c1">PO</th>
+      <th class="c2">BL</th>
+      <th class="c3">Etapa</th>
+      <th class="c4"></th>
     </tr>`
-  minhaListaDeItens.forEach((item, posicao) => {
+    meusProcessos.forEach((item, posicao) => {
     novaLi =
       novaLi +
       `
-        <tr class="task ${item.concluida && 'done'}">
-            <td class="c1"><img class="icone" src="./assets/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})"></td>
-            <td class="nome c2">${item.tarefa}</td>
-            <td class="prazo c3"> ${item.prazo || 'Vazio'}</td>
-            <td class="urgencia c4"> ${item.urgencia || 'Vazio'}</td>
-            <td class="obs c5"> ${item.obs || 'Vazio'}</td>
-            <td class="c6"><img class="icone" src="./assets/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})">
-            <a href="configuracao/config.html?posicao=${posicao}" target="_self">
-              <img class="icone" src="./assets/config.png" alt="configurar-tarefa">
-            </a></td>
+        <tr class="processo ${item.concluido && 'done'}">
+            <td class="po c1">${item.po}</td>
+            <td class="bl c2"> ${item.bl || 'Vazio'}</td>
+            <td class="etapa c3"> ${item.etapa || 'Vazio'}</td>
+            <td class="c4">
+              <img class="icone" src="./assets/trash.png" alt="excluir-processo" onclick="deletarProcesso(${posicao})">
+              <a href="configuracao/painel.html?posicao=${posicao}" target="_self">
+              <img class="icone" src="./assets/config.png" alt="configurar-processo"></a>
+            </td>
         </tr>
       `
   })
   listaCompleta.innerHTML = novaLi
 }
 
-function concluirTarefa(posicao) {
-  minhaListaDeItens[posicao].concluida = !minhaListaDeItens[posicao].concluida
-  localStorage.setItem('minhaListaDeItens', JSON.stringify(minhaListaDeItens))
-  mostrarTarefas()
+function concluirProcesso(posicao) {
+  meusProcessos[posicao].concluido = !meusProcessos[posicao].concluido
+  localStorage.setItem('meusProcessos', JSON.stringify(meusProcessos))
+  mostrarProcessos()
 }
 
-function deletarItem(posicao) {
-  minhaListaDeItens.splice(posicao, 1)
-  localStorage.setItem('minhaListaDeItens', JSON.stringify(minhaListaDeItens))
-  mostrarTarefas()
+function deletarProcesso(posicao) {
+  meusProcessos.splice(posicao, 1)
+  localStorage.setItem('meusProcessos', JSON.stringify(meusProcessos))
+  mostrarProcessos()
 }
 
-button.addEventListener('click', adicionarNovaTarefa)
+button.addEventListener('click', adicionarNovoProcesso)
 
 document.addEventListener('DOMContentLoaded', function() {
   const botaoBaixar = document.querySelector('.button-download-task');
-  botaoBaixar.addEventListener('click', baixarTarefas);
+  botaoBaixar.addEventListener('click', baixarProcessos);
 });
