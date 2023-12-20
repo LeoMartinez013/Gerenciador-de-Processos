ddbbfdb3d8dcf84487a62886f4d3b2a9c72163de
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2023-12-20 10:22:55 -0300

    Comit 6.2
    Correção bug de multiplicação de histórico.

--- configuracao/painel.js
+++ configuracao/painel.js
@@ -89,20 +89,27 @@ window.onload = function() {
   }
 
   const historicosDiv = document.getElementById('historico1').parentNode;
-  for (let i = 0; i < processo.historico.length; i++) {
-    if (i > 0) {
-      adicionarHistorico();
+    for (let i = 0; i < processo.historico.length; i++) {
+      let historico;
+      if (i > 0) {
+        contadorHistorico++;
+        historico = document.getElementById('historico1').cloneNode(true);
+        historico.id = 'historico' + contadorHistorico;
+        const novoHistoricoInputs = historico.getElementsByTagName('input');
+        for (let j = 0; j < novoHistoricoInputs.length; j++) {
+          novoHistoricoInputs[j].value = '';
+        }
+        historicosDiv.appendChild(historico);
+      } else {
+        historico = historicosDiv.getElementsByClassName('historicos')[0];
+      }
+      const dateInput = historico.getElementsByClassName('hist1')[0];
+      const textInput = historico.getElementsByClassName('hist1')[1];
+      const [dateValue, textValue] = processo.historico[i].split(' ');
+      dateInput.value = dateValue;
+      textInput.value = textValue;
     }
-    const historico = historicosDiv.getElementsByClassName('historicos')[i];
-    const dateInput = historico.getElementsByClassName('hist1')[0];
-    const textInput = historico.getElementsByClassName('hist1')[1];
-    const splitHistorico = processo.historico[i].split(' ');
-    const dateValue = splitHistorico.shift();
-    const textValue = splitHistorico.join(' ');
-    dateInput.value = dateValue;
-    textInput.value = textValue;
-  }
-  contadorHistorico = processo.historico.length;
+    contadorHistorico = processo.historico.length;
   
   if (posicao) {
     localStorage.setItem('posicao', posicao);
