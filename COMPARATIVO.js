ccbc50d1411d45b000e6fe07e1c78d6b20802cd0
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2023-12-21 21:13:16 -0300

    comit 7.0.2
    Tentativa

--- COMPARATIVO.js
+++ COMPARATIVO.js
@@ -1,7 +1,77 @@
-window.onload = function() {
-  // A variável 'processo' é definida como um item específico do array 'meusProcessos'
+// Recupera os processos do armazenamento local e os parâmetros da URL
+let meusProcessos = JSON.parse(localStorage.getItem('meusProcessos'));
+const urlParams = new URLSearchParams(window.location.search);
+const posicao = urlParams.get('posicao');
+let contadorHistorico = 1;
+
+// Função para adicionar uma nova linha de histórico
+function adicionarHistorico() {
+  contadorHistorico++;
+  const historicoDiv = document.getElementById('historico1');
+  const novoHistoricoDiv = historicoDiv.cloneNode(true);
+  novoHistoricoDiv.id = 'historico' + contadorHistorico;
+  const novoHistoricoInputs = novoHistoricoDiv.getElementsByTagName('input');
+  for (let i = 0; i < novoHistoricoInputs.length; i++) {
+    novoHistoricoInputs[i].value = '';
+    novoHistoricoInputs[i].id = 'hist' + contadorHistorico + '-' + i;
+    novoHistoricoInputs[i].className = 'hist' + contadorHistorico + ' preenchido';
+  }
+  historicoDiv.parentNode.appendChild(novoHistoricoDiv);
+}
+
+// Função para salvar as configurações do processo
+function salvarConfiguracoes() {
   const processo = meusProcessos[posicao];
-  // Cada linha abaixo preenche um campo de entrada específico no formulário com um valor do objeto 'processo'
+  // Atualiza as propriedades do processo com os valores dos campos de entrada
+  processo.po = document.getElementById(`po`).value;
+  processo.filial = document.getElementById(`filial`).value
+  processo.quantidade = document.getElementById(`quantidade`).value
+  processo.produto = document.getElementById(`produto`).value
+  processo.etb = document.getElementById(`etb`).value
+  processo.navio = document.getElementById(`navio`).value
+  processo.container = document.getElementById(`container`).value
+  processo.bl = document.getElementById(`bl`).value
+  processo.li = document.getElementById(`li`).value
+  processo.lcpo = document.getElementById(`lcpo`).value
+  processo.descarga = document.getElementById(`descarga`).value
+  processo.inspecaoMAPA = document.getElementById(`inspecaoMAPA`).value
+  processo.apresDcts = document.getElementById(`apresDcts`).value
+  processo.deferimento = document.getElementById(`deferimento`).value
+  processo.registroDI = document.getElementById(`registroDI`).value
+  processo.numDI = document.getElementById(`numDI`).value
+  processo.desembaraco = document.getElementById(`desembaraco`).value
+  processo.envNFs = document.getElementById(`envNFs`).value
+  processo.dctsTransporte = document.getElementById(`dctsTransporte`).value
+  processo.mesOperacao = document.getElementById(`mesOperacao`).value
+  processo.etapa = document.getElementById(`etapa`).value
+  // Atualiza o array de histórico com os novos valores dos campos de entrada
+  const historicosDiv = document.getElementById('historico1').parentNode;
+  const historicos = historicosDiv.getElementsByClassName('historicos');
+  processo.historico = []; // Limpa o array historico
+  for (let i = 0; i < historicos.length; i++) {
+    const dateInput = historicos[i].getElementsByClassName('hist' + (i + 1))[0];
+    const textInput = historicos[i].getElementsByClassName('hist' + (i + 1))[1];
+    const result = dateInput.value + " " + textInput.value;
+    console.log(result)
+    // Atualiza a entrada existente ou adiciona uma nova
+    if (i < processo.historico.length) {
+      processo.historico[i] = result;
+    } else {
+      processo.historico.push(result);
+    }
+  }
+
+  // Salva os processos atualizados no armazenamento local
+  localStorage.setItem('meusProcessos', JSON.stringify(meusProcessos));
+
+  const confirmacao = document.getElementById('confirmacao');
+  confirmacao.style.display = 'block';
+  setTimeout(() => confirmacao.style.display = 'none', 3000);
+}
+
+// Função para preencher os campos de entrada com os valores do processo quando a página é carregada
+window.onload = function() {
+  const processo = meusProcessos[posicao];
   document.getElementById(`po`).value = processo.po;
   document.getElementById(`modal`).value = processo.modal
   document.getElementById(`filial`).value = processo.filial
@@ -30,33 +100,26 @@ window.onload = function() {
   } else {
     processo.concluido = false;
   }
+
+  // Preenche os campos de entrada de histórico com os valores do array de histórico
   if (processo.historico) {
-    // Obtém o elemento pai de 'historico1'
     const historicosDiv = document.getElementById('historico1').parentNode;
-    // Preenche o histórico com base no array 'historico' do objeto 'processo'
     for (let i = 0; i < processo.historico.length; i++) {
       let historico;
-      // Cria um novo elemento de histórico se necessário
       if (i > 0) {
         contadorHistorico++;
         historico = document.getElementById('historico1').cloneNode(true);
         historico.id = 'historico' + contadorHistorico;
-        // Limpa os valores dos campos de entrada do novo elemento de histórico
         const novoHistoricoInputs = historico.getElementsByTagName('input');
         for (let j = 0; j < novoHistoricoInputs.length; j++) {
           novoHistoricoInputs[j].value = '';
         }
-        // Adiciona o novo elemento de histórico ao elemento pai
         historicosDiv.appendChild(historico);
       } else {
-        // Obtém o primeiro elemento de histórico existente
         historico = historicosDiv.getElementsByClassName('historicos')[0];
       }
-      // Preenche os campos de data e texto do elemento de histórico
       const dateInput = historico.getElementsByClassName('hist1')[0];
       const textInput = historico.getElementsByClassName('hist1')[1];
-
-      // Divide a string de histórico em 'dateValue' e 'textValue'
       const [dateValue, ...textArray] = processo.historico[i].split(' ');
       const textValue = textArray.join(' ');
 
@@ -64,14 +127,11 @@ window.onload = function() {
       textInput.value = textValue;
     }
   }
-   // Atualiza o valor de 'contadorHistorico' para o número de itens no array 'historico'
   contadorHistorico = processo.historico ? processo.historico.length : 0;
-  
-  // Se a variável 'posicao' estiver definida, armazena seu valor no armazenamento local do navegador
   if (posicao) {
     localStorage.setItem('posicao', posicao);
   }
-  // Adiciona a classe 'preenchido' ou 'nao_preenchido' a cada elemento de entrada na página, dependendo do valor do elemento
+
   const inputs = document.getElementsByTagName('input');
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].value !== "" && inputs[i].value !== "undefined") {
@@ -80,10 +140,28 @@ window.onload = function() {
       inputs[i].classList.add('nao_preenchido');
     }
   }
-  // Se o valor de uma propriedade do objeto 'processo' for vazio ou "undefined", define o valor da propriedade como "vazio"
   for (let prop in processo) {
     if (processo[prop] === "" || processo[prop] === "undefined") {
       processo[prop] = "vazio";
     }
   }
+}
+
+// Função para adicionar a posição ao parâmetro de consulta da URL antes da página ser descarregada
+window.onbeforeunload = function() {
+  const posicao = localStorage.getItem('posicao');
+  if (posicao) {
+    window.location.search = `?posicao=${posicao}`;
+    console.log(`posicao`);
+  }
+}
+
+// Adiciona um ouvinte de evento ao botão 'adicionarHistorico'
+document.getElementById('adicionarHistorico').addEventListener('click', adicionarHistorico);
+
+// Executa a função window.onload quando a página é carregada
+if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+  window.onload();
+} else {
+  document.addEventListener("DOMContentLoaded", window.onload);
 }
